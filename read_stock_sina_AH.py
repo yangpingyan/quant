1615d9ff950d878b9ba48d80f8466b0d85ead050
yangpingyan <yangpingyan@gmail.com>
2018-10-22 00:51:15 +0800

    1

--- read_stock_sina_AH.py
+++ read_stock_sina_AH.py
@@ -2,4 +2,16 @@
 # -*- coding: utf-8 -*- 
 # @Time : 2018/10/21 20:21 
 # @Author : yangpingyan@gmail.com
+import urllib.request
+from bs4 import BeautifulSoup
 
+response = urllib.request.urlopen("http://finance.sina.com.cn/stock/hkstock/anh.shtml")
+html = response.read().decode("gbk")
+soup = BeautifulSoup(html)
+print(soup.prettify())
+div = soup.find_all( id = 'table_container')
+a_bf = BeautifulSoup(str(div[0]))
+a = a_bf.find_all('a')
+for each in a:
+    print(each.string, each.get('href'))
+    break
