1db5b62bde0cddf7546b8afd417fb4b18332b169
yangpingyan <yangpingyan@gmail.com>
2018-10-23 01:00:02 +0800

    1

--- read_stock_sina_AH.py
+++ read_stock_sina_AH.py
@@ -5,8 +5,6 @@
 import re
 import urllib
 
-from bs4 import BeautifulSoup
-
 url_pre = 'http://hq.sinajs.cn/rn=&list='
 # code = 'rt_hkhqtime,s_sh000001,s_sz399001,rt_hkHSI,rt_hkHSCEI,HKDCNY,s_sh000905,s_sh000300,sh601318,sz000001'
 code = 'sh601318'
@@ -14,29 +12,16 @@ url = url_pre + code
 
 response = urllib.request.urlopen(url)
 rep_data = response.read().decode("gbk")
-
 stocks_detail = "".join(rep_data)
 grep_str = re.compile(r"(\d+)=([^\s][^,]+?)%s%s" % (r",([\.\d]+)" * 29, r",([-\.\d:]+)" * 2))
+grep_str = re.compile(r"(\d+)=([^\s][^,]+?)" )
 result = grep_str.finditer(stocks_detail)
 all = grep_str.findall(stocks_detail)
 len(all)
-soup = BeautifulSoup(html)
-print(soup.prettify())
-/ html / body / div[3]
-body > div.i_bd.w95.i_clear.i_allbg
-div = soup.find_all(class_='i_bd w95 i_clear i_allbg')
-div
-a_bf = BeautifulSoup(str(div[0]))
-a = a_bf.find_all('a')
-for each in a:
-    print(each.string, each.get('href'))
-    break
 
-scrapy
-startproject
-tutorial
-
-scrapy
-shell
-http: // finance.sina.com.cn / stock / hkstock / anh.shtml
-response.xpath(r'//*[@id="table_container"]').extract()
+url_ahstocks = 'http://vip.stock.finance.sina.com.cn/quotes_service/api/jsonp.php/var%20hk_corrastock=/HK_CorrStock.getCorrstock'
+response = urllib.request.urlopen(url_ahstocks)
+rep_data = response.read().decode("gbk")
+stocks_detail = "".join(rep_data)
+grep_str = re.compile(r"(\w+)=\(\(")
+all = grep_str.findall(stocks_detail)
